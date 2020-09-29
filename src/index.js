8a7b08bbab45edf3d2e0cd7f228133f736019c8d
YaroslavTrefilov <yaroslavtrefilov5@gmail.com>
2020-09-29 19:48:15 +0400

    feat: implement 3 function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0 )) {
+    return 0;
+  } else {
+  let sortedArray = array.sort(function(a, b) {
+      return a - b;
+  })
+  return sortedArray[0];
+  }
 }
 
+
+
+
+
+
+
+
+
+
 exports.max = function max (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0 )) {
+    return 0;
+  } else {
+  let sortedArray = array.sort(function(a, b) {
+      return a - b;
+  })
+  return sortedArray[array.length - 1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0 )) {
+    return 0;
+  } else {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let result = array.reduce(reducer);
+    result /= array.length;
+    return result
+  }
 }
